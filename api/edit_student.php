eb9dc257c86e7cb61370009b64d50a265fe85b21
unknown <dullstart.yang.com>
2022-11-21 00:15:06 +0800

    註解比code還多 > <

--- api/edit_student.php
+++ api/edit_student.php
@@ -37,15 +37,28 @@ $sql_students="UPDATE `students`
 //學員所屬班級在另一張資料class_student
 $class_code =$_POST['class_code'];
 //建立相關表格------------------------------------------------
+//因為`studends`.`school_num`並未傳送過來(當初用計算+1)，
+//所以要建立，一旦建立，可以抓`class_student`.`{column}`
 $school_num=$pdo->query("SELECT * FROM `students` WHERE `id`='$id'")
             ->fetch(PDO::FETCH_ASSOC);
+//上述code，自己解釋 抓取students裡的所有表個，以id為主的那一行欄位
 //$id=$_POST['id']從 add.php >>> edit.php(建立隱藏欄位) >>> this(edit_student.php)
 
+//透過可以抓取`students`.`all_columns`，以查詢語法建立以 id 為主的 `class_student`相關資料
+//其共同欄位是 `school_num`
 $class= $pdo->query("SELECT * FROM `class_student` WHERE `school_num`='{$school_num['school_num']}'")
             ->fetch(PDO::FETCH_ASSOC);
 //相關表格建立完成--------------------------------------------
 
 $sql_class_student="UPDATE `class_student` SET `class_code`='$class_code' WHERE `id`='{$class['id']}'";
+//$sql_class_student="UPDATE `class_student` SET `class_code`='$class_code' WHERE `school_num`='{$school_num['school_num']}'";
+//更新相關資料表`class_student`裡的欄位`class_code`
+//$class_code來自 `classes`.`id`
+//條件 : 欄位名id = 
+//$class['id']解釋為`students`.`school_num`(id帶過來的) 與 `class_student`.`school_num`同個相關連動的那行的`id`
+//id 不修改，要改的是 `class_student`.`class_code`
+//WHERE `id`='{$class['id']}' 可以改成 WHERE `school_num`='{$school_num['school_num']}'
+
 echo $sql_students;
 echo "<br>";
 echo $sql_class_student;
@@ -53,4 +66,11 @@ echo "<br>";
 $res1=$pdo->exec($sql_students);
 $res2=$pdo->exec($sql_class_student);
 echo "編輯成功 !!";
+if($res1 || $res2){
+    $edit='edit_success';
+}else{
+    $edit='edit_fail';
+}
+echo $edit;
+header("location:../edit.php?id=$id&edit=$edit");
 ?>
--- edit.php
+++ edit.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="css/form.css">
     <title>修改學生資料</title>
     <style>
-        form{
+        form {
             height: 680px;
         }
     </style>
@@ -18,44 +18,44 @@
 <body>
     <h1>修改學生資料</h1>
     <?php
-    if(isset($_GET['id'])){
-    $sql = "SELECT * FROM `students` WHERE `id`='{$_GET['id']}'";
-    $student = $pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
-    }else{
+    if (isset($_GET['id'])) {
+        $sql = "SELECT * FROM `students` WHERE `id`='{$_GET['id']}'";
+        $student = $pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
+    } else {
         header("location:index.php?status=edit_error");
     }
-       
+
     ?>
     <form action="api/edit_student.php" method="post">
         <label>學　　號 :
-            <span><?=$student['school_num']?></span>   
+            <span><?= $student['school_num'] ?></span>
         </label>
         <label>姓　　名 :
-            <input type="text" name="name" value="<?=$student['name']?>">
+            <input type="text" name="name" value="<?= $student['name'] ?>">
         </label>
         <label>生　　日 :
-            <input type="date" name="birthday" value="<?=$student['birthday']?>">
+            <input type="date" name="birthday" value="<?= $student['birthday'] ?>">
         </label>
         <label>身分證號 :
-            <input type="text" name="uni_id" value="<?=$student['uni_id']?>">
+            <input type="text" name="uni_id" value="<?= $student['uni_id'] ?>">
         </label>
         <label>住　　址 :
-            <input type="text" name="addr" value="<?=$student['addr']?>">
+            <input type="text" name="addr" value="<?= $student['addr'] ?>">
         </label>
         <label>家長姓名 :
-            <input type="text" name="parents" value="<?=$student['parents']?>">
+            <input type="text" name="parents" value="<?= $student['parents'] ?>">
         </label>
         <label>連絡電話 :
-            <input type="text" name="tel" value="<?=$student['tel']?>">
+            <input type="text" name="tel" value="<?= $student['tel'] ?>">
         </label>
         <label>科　　別 :
             <select name="dept">
                 <?php
                 //從`dept`資料表中撈出所有的科系資料並在網頁上製作成下拉選單的項目
-                $sql="SELECT * FROM `dept`";
-                $depts=$pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
-                foreach($depts as $dept){
-                    $selected=($dept['id']==$student['dept'])?'selected':'';
+                $sql = "SELECT * FROM `dept`";
+                $depts = $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+                foreach ($depts as $dept) {
+                    $selected = ($dept['id'] == $student['dept']) ? 'selected' : '';
                     echo "<option value='{$dept['id']}' $selected>{$dept['name']}</option>";
                 }
                 ?>
@@ -65,10 +65,10 @@
             <select name="graduate_at">
                 <?php
                 //從`graduate_school`t資料表中撈出所有的畢業學生資料並在網頁上製作成下拉選單的項目
-                $sql="SELECT * FROM `graduate_school` ";
-                $grads=$pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
-                foreach($grads as $grad){
-                    $selected=($grad['id']==$student['graduate_at'])?'selected':'';
+                $sql = "SELECT * FROM `graduate_school` ";
+                $grads = $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+                foreach ($grads as $grad) {
+                    $selected = ($grad['id'] == $student['graduate_at']) ? 'selected' : '';
                     echo "<option value='{$grad['id']}' $selected>{$grad['county']}{$grad['name']}</option>";
                 }
                 ?>
@@ -78,10 +78,10 @@
             <select name="status_code">
                 <?php
                 //從`status`資料表中撈出所有的畢業狀態並在網頁上製作成下拉選單的項目
-                $sql="SELECT * FROM `status`";
-                $rows=$pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
-                foreach($rows as $row){
-                    $selected=($row['code']==$student['status_code'])?'selected':'';
+                $sql = "SELECT * FROM `status`";
+                $rows = $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+                foreach ($rows as $row) {
+                    $selected = ($row['code'] == $student['status_code']) ? 'selected' : '';
                     echo "<option value='{$row['code']}' $selected>{$row['status']}</option>";
                 }
                 ?>
@@ -94,27 +94,40 @@
                 //class_student 與 classes 有相同的資料
                 //`students`.`id`從form表單過來，要建立class_student與students的連結
                 //相同欄位 >>> school_num <<< 
-                $stu_class=$pdo->query("SELECT *  FROM `class_student`  WHERE `school_num`='{$student['school_num']}'")->fetch(PDO::FETCH_ASSOC);
+                $stu_class = $pdo->query("SELECT *  FROM `class_student`  WHERE `school_num`='{$student['school_num']}'")->fetch(PDO::FETCH_ASSOC);
                 //這樣表單`class_student`與 一開始id過來的單筆資料 已產生連結 
                 //兩張表格`class_student` and `students` 的column都抓得到了!!
 
                 //從`classes`資料表中撈出所有的班級資料並在網頁上製作成下拉選單的項目
-                $sql="SELECT `id`,`code`,`name` FROM `classes`";
-                $rows=$pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
-                foreach($rows as $row){
-                    $selected=($row['code']==$stu_class['class_code'])?'selected':'';
+                $sql = "SELECT `id`,`code`,`name` FROM `classes`";
+                $rows = $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+                foreach ($rows as $row) {
+                    $selected = ($row['code'] == $stu_class['class_code']) ? 'selected' : '';
                     echo "<option value='{$row['code']}' $selected>{$row['name']}</option>";
                 }
                 ?>
             </select>
         </label>
-        <label>座　　號 : 
-            <span>　<?=$stu_class['seat_num'] ?></span>
+        <label>座　　號 :
+            <span>　<?= $stu_class['seat_num'] ?></span>
         </label>
         <!-- 建立 id 修改需用到 -->
-        <input type="hidden" name="id" value="<?=$student['id']?>">
+        <input type="hidden" name="id" value="<?= $student['id'] ?>">
         <input type="submit" value="確認修改">
     </form>
+    <a href="index.php?page=1" class="back">回列表</a>
+    <?php
+    if (isset($_GET['edit'])) {
+        switch ($_GET['edit']) {
+            case 'edit_success':
+                echo "<div class='edit'>修改成功</div>";
+                break;
+            case 'edit_fail';
+                echo "<div class='edit edit_err'>修改有誤或未修改</div>";
+                break;
+        }
+    }
+    ?>
 </body>
 
 </html>

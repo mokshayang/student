61a34c83021c4cd874b6f904388b7ef11868cc99
unknown <dullstart.yang.com>
2022-12-04 18:53:57 +0800

    前端的新聞，新功能 nl2br

--- front/main.php
+++ front/main.php
@@ -1,1 +1,76 @@
-<h1>do what</h1>
+<style>
+    .group ,.list{
+        width: 68%;
+        margin: auto;
+        display: grid;
+        grid-template-columns: 8fr 2fr;
+        align-items: center;
+    }
+    .group{
+        justify-items: center;
+        font-size: 24px;
+        background-color: #333;
+        height: 48px;
+        color: #eee;
+        border-radius: 15px 15px 0 0;
+    }
+    .list{
+        grid-auto-rows: 40px;
+        font-size: 20px;
+    }
+    .list:nth-child(2n+1){
+        background-color: #eef;
+    }
+    li div:nth-child(1){
+        padding-left:20px ;
+        color: #f00;
+        text-shadow: 1px 1px 1px #f00;
+    }
+    li div:nth-child(2){
+        justify-self:center;
+    }
+    li a{
+        color: #00e;
+        text-decoration: none;
+    }
+    li a:hover{
+        color: #f00;
+        text-decoration: underline;
+    }
+   
+</style>
+<h1>最新消息</h1>
+<ul>
+<li class="group">
+    <div class="subject">標 題</div>
+    <div class="people">人 氣</div>
+</li>
+</ul>
+<?php
+$all_news="SELECT * FROM `news` ORDER BY `top` DESC, `readed` DESC";
+$rows=$pdo->query($all_news)->fetchALL(PDO::FETCH_ASSOC);
+// echo "<pre>";
+// print_r($rows[0]);
+// echo "</pre>";
+//點閱數量查詢的正排序 :
+$hot=$pdo->query("SELECT `id` FROM `news` ORDER BY `readed` DESC")->fetchColumn();
+
+foreach($rows as $row){
+    echo "<li class='list'>";
+    echo "<div class='title'>";
+    echo ($row['top']==1)?"TOP&nbsp;&nbsp;":'';
+    echo ($row['id']==$hot)?"HOT&nbsp;&nbsp;":'';
+    echo "<a href='index.php?do=news_detail&id={$row['id']}'>";
+    echo $row['subject'];
+    echo "</a>";
+    echo "</div>";
+    echo "<div>";
+    echo $row['readed'];
+    echo "</div>";
+    echo "</li>";
+//     echo "<pre>";
+// print_r($row);
+// echo "</pre>";
+}
+
+?>
---
+++ front/news_detail.php
@@ -0,0 +1,38 @@
+<style>
+    .total{
+        width: 72%;
+        min-width:520px;
+        margin: auto;
+    }
+    h2{
+        margin: 10px;
+        font-size: 36px;
+    }
+    .timer{
+        text-align:right;
+        padding-right:40px;
+    }
+    .type{
+        margin: 10px auto;
+        font-size: 24px;
+    }
+    .content{
+        font-size: 20px;
+        background-color: #eef;
+        padding:10px;
+    }
+</style>
+<?php
+
+$sql = "SELECT * FROM `news` WHERE `id`='{$_GET['id']}'";
+$news = $pdo->query($sql)->fetch();
+
+?>
+<div class="total">
+    <h2><?= $news['subject'] ?></h2>
+    <div class="timer">
+        發布時間 : <?= $news['created_at'] ?>
+    </div>
+    <div class="type">[ <?= $news['type']  ?> ] : </div>
+    <div class="content"><?= nl2br($news['content']) ?></div>
+</div>

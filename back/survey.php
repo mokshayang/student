e35450d527ef2fd0d226282eea0c928af6faea27
unknown <dullstart.yang.com>
2022-12-11 23:25:07 +0800

    back.survey_add.php

---
+++ back/survey.php
@@ -0,0 +1,31 @@
+<h3 >調查列表</h3>
+<div class=add_head>
+    <a href="admin_center.php?do=survey_add">新增調查主題</a>
+</div>
+
+<div class="table_list">
+    <div class="table_head">
+        <div>主題</div>
+        <div>參與數</div>
+        <div>操作</div>
+    </div>
+    <?php
+    $surveys = all("survey_subject");
+    // dd($surveys);
+    foreach ($surveys as $survey) {
+    ?>
+        <div class="items">
+            <div><?= $survey['subject'] ?></div>
+            <div><?= $survey['vote'] ?></div>
+            <div>
+                <?php
+                $activeBg = ($survey['active'] == 1) ? "btn-primary" : "btn-secondary";
+                $activeText = ($survey['active'] == 1) ? "啟用" : "關閉";
+                ?>
+                <a href="./api/survey_active.php?id=<?= $survey['id'] ?>" class="btn btn-sm mx-1 <?= $activeBg ?> "><?= $activeText ?></a>
+                <a href="./admin_center.php?do=survey_edit&id=<?=$survey['id'];?>" class="btn btn-sm btn-success mx-1">編輯</a>
+                <a href="./api/survey_del.php?id=<?=$survey['id']?>" class="btn btn-sm btn-info mx-1">刪除</a>
+            </div>
+        </div>
+    <?php }; ?>
+</div>
---
+++ back/survey_add.php
@@ -0,0 +1,113 @@
+<style>
+    h3 .subject_add {
+        margin-left: 20px;
+        border-radius: 8px;
+        height: 36px;
+        width: 36px;
+        line-height: 28px;
+        background-color: #44992280;
+        border: none;
+        color: #fff;
+    }
+    h3 .subject_add:hover{
+        background-color: #44992298;
+    }
+
+    form {
+        width: 68%;
+        margin: 32px auto;
+    }
+    form .subject{
+        margin: auto;
+    }
+    form .options{
+       margin: auto;
+        
+    }
+    form .option , form .subject{
+        display: grid;
+        grid-auto-rows: 48px;
+        justify-items: center;
+    }
+
+    .ss {
+        padding: 5px;
+        border-radius: 5px;
+        outline: 1px solid #fff;
+        border: 1px solid pink;
+        width: 480px;
+    }
+
+    label input:hover {
+        border: 3px solid #bbf;
+        border-radius: 6px;
+    }
+    .cc{
+        
+        color:#33f;
+        font-weight: bold;
+    }
+    @media only screen and (max-width: 780px) {
+        form{width:90%;}
+        .ss {
+            width: 360px;
+        }
+    }
+</style>
+<h3>新增調查<button onclick="addOption()" class="subject_add" title="增加選項">+</button></h3>
+<form action="./api/survey_add.php" method="post">
+    <div class="subject">
+        <label class="cc" style="margin-bottom: 12px;"> 主題 &nbsp;
+            <input type="text" name="subject" required class="ss">
+        </label>
+    </div>
+    <!-- 選項區 -->
+    <div id="options">
+        <div class="option">
+            <label class="cc">選項1
+                <input type="text" name="opt[]" required class="ss">
+            </label>
+        </div>
+    </div>
+
+    <div class="text-center col-12 mt-3">
+        <input class="btn btn-warning mx-1" type="reset" value="重置">
+        <input class="btn btn-primary mx-1" type="submit" value="確定新增">
+    </div>
+    
+</form>
+<script>
+    function addOption() {
+        let options = document.getElementById('options');
+        let num = document.getElementsByClassName('option').length + 1
+        // console.log(mun);
+        /*     
+        let opt=`<div class="option form-group row col-12">
+                    <label class="col-2 text-right">項目${num}</label>
+                    <input type="text" name="opt[]" class="form-control col-10">
+                </div>` 
+        */
+        //createElement
+        let opt = document.createElement("div");
+        let label = document.createElement("label");
+        let input = document.createElement('input');
+        let numNode = document.createTextNode("選項" + num + " ");
+        console.log(numNode);
+        //setAttribute :第一個參數是你要修改的屬性名稱，第二個是要改成的內容
+        opt.setAttribute("class", "option") //設定 opt 的 class 屬定
+        input.setAttribute("class", "ss")
+        label.setAttribute("class", "cc");
+        // opt.setAttribute("class", "option form-group row col-12")
+        input.setAttribute("name", "opt[]");
+        input.setAttribute("type", "text");
+        //appendChild :
+        label.appendChild(numNode);
+        opt.appendChild(label);
+        label.appendChild(input);
+
+        options.appendChild(opt);
+        //options.innerHTML=options.innerHTML+opt
+        //console.log(options.innerHTML)
+    }
+    
+</script>
